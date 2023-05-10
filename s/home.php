d95407e113b07f0eb39edd2d9b3de3230a88eb5f
Kunj Patel <kunjd26@hotmail.com>
2023-05-10 09:47:55 +0530

    add some basic css in all home pages

--- s/home.php
+++ s/home.php
@@ -10,11 +10,65 @@ try {
 }
 ?>
 
-<div class="container" style="min-height: 100vh">
+<style>
+/* Internal CSS */
+h1, h4 {
+    font-weight: bold;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+}
+
+.lead {
+    font-size: 1.2rem;
+    line-height: 1.5;
+}
+
+.col-md-6 {
+    margin-top: 40px;
+}
+
+/* Inline CSS */
+h1 {
+    animation: slide-in-fwd-center 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+}
+
+@keyframes slide-in-fwd-center {
+  0% {
+    transform: translateZ(-1400px) rotateX(90deg);
+    opacity: 0;
+  }
+  100% {
+    transform: translateZ(0) rotateX(0);
+    opacity: 1;
+  }
+}
+
+</style>
+
+<div class="container" style="min-height: 100vh;">
     <div class="row">
-        <h1>Seller-Home</h1>
-        <h1><?= $_SESSION["user_id"] ?></h1>
-        <?php print_r($data) ?>
+        <h1 class="mb-4">Distributor-Home</h1>
+        <h1 class="mb-4"><?= $_SESSION["user_id"] ?></h1>
+        
+        <div class="col-md-6">
+            <h4 class="mb-2">Name:</h4>
+            <p class="lead"><?= $data["name"] ?></p>
+
+            <h4 class="mb-2">Address:</h4>
+            <p class="lead"><?= $data["address"] ?></p>
+
+            <h4 class="mb-2">Email:</h4>
+            <p class="lead"><?= $data["email"] ?></p>
+        </div>
+
+        <div class="col-md-6">
+            <h4 class="mb-2">Amount:</h4>
+            <p class="lead"><?= $data["amount"] ?></p>
+
+            <h4 class="mb-2">Total Connected Providers:</h4>
+            <p class="lead"><?= $data["total_connected_provider"] ?></p>
+        </div>
+    
     </div>
 </div>
 
